d47359e6742b8a2ec41e9aae18203f737035975e
GroovieGermanikus <groovie@mango.markets>
2023-05-04 09:26:47 +0200

    switch to ETH-PERP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::cmp::Ordering;
+use std::fmt::format;
 use std::iter;
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
-use serde_json::{json, to_writer};
+use serde_json::{from_str, json, to_writer, Value};
 use tokio::net::TcpStream;
 use tokio_tungstenite::{connect_async, tungstenite};
 use tokio_tungstenite::tungstenite::{client, connect, Message, WebSocket};
@@ -12,6 +13,7 @@ use tokio_tungstenite::tungstenite::stream::MaybeTlsStream;
 use url::Url;
 
 mod services;
+mod mango;
 
 #[tokio::main]
 async fn main() {
@@ -24,9 +26,37 @@ async fn main() {
 
 }
 
+// mango-feeds
+pub type OrderbookLevel = [f64; 2];
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
-pub struct UiAccount {
+pub enum OrderbookSide {
+    Bid = 0,
+    Ask = 1,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct OrderbookUpdate {
+    pub market: String,
+    pub side: OrderbookSide,
+    pub update: Vec<OrderbookLevel>,
+    pub slot: u64,
+    pub write_version: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct OrderbookCheckpoint {
+    pub market: String,
+    pub bids: Vec<OrderbookLevel>,
+    pub asks: Vec<OrderbookLevel>,
+    pub slot: u64,
+    pub write_version: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct WsSubscription {
     pub command: String,
     pub marketId: String,
 }
@@ -38,27 +68,84 @@ pub struct Subscriptions {
     // pub marketIds: String,
 }
 
+// requires running "service-mango-orderbook"
+//
+// cargo run --bin service-mango-orderbook service-mango-orderbook/conf/test-config.toml
 fn listen_orderbook_feed() {
 
     let (mut socket, response) =
         connect(Url::parse("ws://127.0.0.1:8080").unwrap()).expect("Can't connect");
 
+    if response.status() != 101 {
+        // TODO implement reconnects
+        panic!("Error connecting to the server: {:?}", response);
+    }
+    // Response { status: 101, version: HTTP/1.1, headers: {"connection": "Upgrade", "upgrade": "websocket", "sec-websocket-accept": "ppgfXDDxtQBmL0eczLMf5VGbFIo="}, body: () }
+
     // subscriptions= {"command":"subscribe","marketId":"ESdnpnNLgTkBCZRuTJkZLi5wKEZ2z47SG3PJrhundSQ2"}
-    let json = serde_json::to_string(
-        &UiAccount {
-            command: "subscribe".to_string(),
-            marketId: "ESdnpnNLgTkBCZRuTJkZLi5wKEZ2z47SG3PJrhundSQ2".to_string(),
-        }).unwrap();
-    println!("json {:?}", json);
+    let sub = &WsSubscription {
+        command: "subscribe".to_string(),
+        marketId: mango::MARKET_ETH_PERP.to_string(),
+    };
     // Ok(Text("{\"success\":false,\"message\":\"market not found\"}"))
     // Ok(Text("{\"success\":true,\"message\":\"subscribed\"}"))
 
-
-    socket.write_message(Message::text(json));
+    socket.write_message(Message::text(json!(sub).to_string()));
 
     loop {
-        let xx = socket.read_message();
-        println!("xx {:?}", xx);
+        match socket.read_message() {
+            Ok(msg) => {
+                println!("Received: {}", msg);
+            }
+            Err(e) => {
+                match e {
+                    tungstenite::Error::ConnectionClosed => {
+                        println!("Connection closed");
+                        break;
+                    }
+                    _ => {}
+                }
+                println!("Error reading message: {:?}", e);
+                break;
+            }
+        }
+        let msg = socket.read_message().unwrap();
+
+        let msg = match msg {
+            tungstenite::Message::Text(s) => { s }
+            _ => continue
+        };
+
+        let plain = from_str::<Value>(&msg).expect("Can't parse to JSON");
+
+        // detect checkpoint messages via property bid+ask
+        let is_checkpoint_message = plain.get("bids").is_some() && plain.get("asks").is_some();
+        // detect update messages
+        let is_update_message = plain.get("update").is_some();
+
+
+        if (is_checkpoint_message) {
+            let checkpoint: OrderbookCheckpoint = serde_json::from_value(plain.clone()).expect("");
+            // println!("chkpt bids: {:?}", checkpoint.bids);
+            println!("chkpt asks: {:?}", checkpoint.asks);
+        }
+
+        if is_update_message {
+            let update: OrderbookUpdate = serde_json::from_value(plain.clone()).expect(format!("Can't convert json <{}>", msg).as_str());
+            // println!("{:?}", msg);
+            // let update: OrderbookUpdate = from_str::<OrderbookUpdate>(&msg).expect("Can't parse to JSON");
+            if update.side == OrderbookSide::Ask {
+                println!("update({:?}): {:?}", update.slot, update.update);
+            }
+        }
+
+
+
+        // Ok(Text("{\"market\":\"ESdnpnNLgTkBCZRuTJkZLi5wKEZ2z47SG3PJrhundSQ2\",\"side\":\"ask\",\"update\":[[21.62,0.0],[21.63,1.32]],\"slot\":191923195,\"write_version\":695276659185}"))d
+        // OrderbookLevel {
+        //     price: 21.62,
+        //     quantity: 0.0,
+        // }
     }
 
 
---
+++ src/mango.rs
@@ -0,0 +1,5 @@
+
+pub const MARKET_SOL_PERP: &str = "ESdnpnNLgTkBCZRuTJkZLi5wKEZ2z47SG3PJrhundSQ2";
+pub const MARKET_BTC_PERP: &str = "HwhVGkfsSQ9JSQeQYu2CbkRCLvsh3qRZxG6m4oMVwZpN";
+pub const MARKET_ETH_PERP: &str = "Fgh9JSZ2qfSjCw9RPJ85W2xbihsp2muLvfRztzoVR7f1";
+
